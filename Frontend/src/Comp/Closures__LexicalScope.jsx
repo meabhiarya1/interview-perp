73187f51084b485f58d5cf485cbea90bc2c812e2
Abhishek kumar <abhi.arya97161@gmail.com>
2025-02-01 16:35:13 +0530

    Update Closures__LexicalScope component to return inner function output and log closure behavior

--- Frontend/src/Comp/Closures__LexicalScope.jsx
+++ Frontend/src/Comp/Closures__LexicalScope.jsx
@@ -4,12 +4,15 @@ const Closures__LexicalScope = () => {
   function outerFunction() {
     let name = "Outer";
     function innerFunction() {
-      console.log(name + " + Inner Lexical Envirnoment");
+      return name + " + Inner Lexical Envirnoment";
     }
     return innerFunction;
   }
   const innerFunc = outerFunction();
-  innerFunc();
+  console.log(innerFunc); // Output: [Function: innerFunction] and 
+  // // Even though the outerFunction has returned, the innerFunction still has access to the outerFunction's variables this is called closure.
+  console.log(innerFunc()); // Output: Outer + Inner Lexical Envirnoment and 
+  // // Even though the innerFunction has returned, the outerFunction still has access to the innerFunction's variables this is called closure.
 
   return <div>Closures__LexicalScope</div>;
 };
